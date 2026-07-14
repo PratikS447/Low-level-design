c0964072befd4345608804fe611b4852fcff008f
PratikS447 <shindepratik447@gmail.com>
2026-07-14 14:55:56 +0530

    Generics beginner problem

---
+++ src/main/java/org/example/LLD/generics_need/Risky_and_Needs.java
@@ -0,0 +1,24 @@
+package org.example.LLD.generics_need;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Risky_and_Needs {
+    public static void main(String args[]){
+        List ls= new ArrayList<>();
+        ls.add("Pratik");
+        ls.add(45);
+
+        // if try to get the numeric value and put String as its conatiner name it will give the complile time error.
+        // Eventhrought the code will compile at runtime.
+
+        String a = (String) ls.get(0); // here we explicitly require type-cast -> ClassCastException
+        System.out.println(a);
+
+        // if we explicitly use
+        List<String> ls2 = new ArrayList<>();
+        ls2.add("Pratik");
+//        ls2.add(123); -> thow an runtime error
+
+    }
+}
